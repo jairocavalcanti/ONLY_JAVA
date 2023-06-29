c1b6182a7e12652ca7e376cf3d7bcf09addd9fd6
Jairo <jairoregiscavalcanti@gmail.com>
2023-06-29 19:31:08 -0300

    curso java

--- LAMBDA/Pratico3/Principal.java
+++ LAMBDA/Pratico3/Principal.java
@@ -4,38 +4,37 @@ import java.util.Scanner;
 import java.util.ArrayList;
 
 public class Principal {
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        ArrayList<Double> array = new ArrayList<> ();
+        ArrayList<Double> array = new ArrayList<>();
+        ArrayList<Double> array2 = new ArrayList<>();
 
-        System.out.println("Insira a quantidade de elementos double que vai inserir na lista: ");
+        System.out.println("Insira a quantidade de elementos do array double: ");
         double quantidade = scanner.nextDouble();
-        
-        for(int i = 0; i< quantidade; i++){
-            System.out.println("Insira o elemento " + i + "do arraylist: ");
-            Double elemento = scanner.nextDouble();
-            array.add(elemento);
+
+        for(int i = 0;i < quantidade;i++){
+          double elemento = scanner.nextDouble();
+          array.add(elemento);
         }
 
-        System.out.println(array);
+       System.out.println(array);
 
-        System.out.println("Deseja adicionar algum numero aos elementos do array? ");
-        String resposta = scanner.next();
-        
-        if(resposta.equals("S")){
-            System.out.println("Insira o numero escolhido para soma:");
-            Double soma = scanner.nextDouble();
-        }
+       System.out.println("Insira numero para soma: ");
+       double soma = scanner.nextDouble();
 
+       array.forEach((v) -> {array2.add(v + soma);});
 
+       System.out.println("Elementos do array somados a variavel soma: ");
+       System.out.println(array2);
 
 
-        array.forEach((v) -> { });
 
         scanner.close();
     }
-    
+
+
      /*
         valores.forEach((v) -> {
         dobro.add(v*2);
@@ -46,5 +45,6 @@ public class Principal {
          }
     });*/
 
+    // Consumer<Double> consumer = ((E) -> {arraysoma.add(E + soma);});
 
 }
