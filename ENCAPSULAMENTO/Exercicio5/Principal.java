27b50fcbfd2b9ca0384f4b99a760c16c4452ba97
Jairo <jairoregiscavalcanti@gmail.com>
2023-07-13 10:20:47 -0300

    Updates Encapsulamento e Metodos

--- ENCAPSULAMENTO/Exercicio5/Principal.java
+++ ENCAPSULAMENTO/Exercicio5/Principal.java
@@ -5,7 +5,6 @@ public class Principal {
     public static void main(String[] args) {
         Player p = new Player();
 
-        System.out.println("----------------------");
         p.ligarconsole();
         p.rodarjogo();
         p.desligarconsole();
