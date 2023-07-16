36df8245b28d41f10c982de0a7a1b4c9e6aae124
Jairo <jairoregiscavalcanti@gmail.com>
2023-07-15 21:36:31 -0300

    Updates encapsulamento

--- ENCAPSULAMENTO/Exercicio1/Principal.java
+++ ENCAPSULAMENTO/Exercicio1/Principal.java
@@ -1,10 +1,13 @@
 package ENCAPSULAMENTO.Exercicio1;
 
 public class Principal {
+
+    //Programa para pratica de ENCAPSULAMENTO em java
+    //Metodos abaixo irão se comportar de formas diferentes conforme a ordem
+
     public static void main(String[] args) {
     
     PainelUsina panel = new PainelUsina();
-   
    
     panel.ligar();
     panel.desligar();   
