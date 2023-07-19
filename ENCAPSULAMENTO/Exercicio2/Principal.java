20bf96f0bc93682630dbda403f3fa3486d0106cd
Jairo <jairoregiscavalcanti@gmail.com>
2023-07-19 00:32:24 -0300

    Updates Encapsulamento

--- ENCAPSULAMENTO/Exercicio2/Principal.java
+++ ENCAPSULAMENTO/Exercicio2/Principal.java
@@ -3,10 +3,16 @@ package ENCAPSULAMENTO.Exercicio2;
 public class Principal {
     public static void main(String[] args) {
         ControleRemoto c = new ControleRemoto();
+        
         c.ligar();
+        c.play();
+        c.maisvolume();
+        c.maisvolume();
         c.abrirmenu();
-        c.menosvolume();
-        c.fecharmenu();
+       
+
+
+    
     
     
     } 
