b1da14b7bc6a60a13d95161a9cd597a47fb06f2a
Jairo <jairoregiscavalcanti@gmail.com>
2023-10-19 09:23:51 -0300

    Exercicio1_inserido

---
+++ ESTRUTURA_SEQUENCIAL/Exercicio1/Principal.java
@@ -0,0 +1,11 @@
+package ESTRUTURA_SEQUENCIAL.Exercicio1;
+
+/* Faça um Programa que mostre a mensagem "Alo mundo" na tela */
+
+public class Principal {
+    public static void main(String[] args) {
+        System.out.println("Olá mundo!");
+    
+    }
+    
+}
