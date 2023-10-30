c99938298128f2e800ed460ca5281a6c6d1bc850
Jairo <jairoregiscavalcanti@gmail.com>
2023-10-30 16:05:06 -0300

    Exercicio_5_inserido

--- ESTRUTURA_DE_REPETIÇÃO/Exercicio5/Principal.java
+++ ESTRUTURA_DE_REPETIÇÃO/Exercicio5/Principal.java
@@ -1,4 +1,5 @@
 package ESTRUTURA_DE_REPETIÇÃO.Exercicio5;
+import java.util.Locale;
 import java.util.Scanner;
 
 /*  
@@ -11,32 +12,35 @@ public class Principal {
       Scanner scanner = new Scanner(System.in);
       int anos = 0;  
 
+      Locale.setDefault(Locale.US);
+
       System.out.println("Insira a população do país A:  ");
       double populaçaoA = scanner.nextDouble();
 
       System.out.println("Insira a população do país B:");
       double populaçaoB = scanner.nextDouble();
 
-      System.out.println("Insira a porcentagem de crescimento populacional do país A: ");
+      System.out.println("Insira a porcentagem de crescimento populacional por ano do país A: ");
       double porcentagemA = scanner.nextDouble();
 
-      double valorporcentagemA = (populaçaoA * porcentagemA) / 100;
+     // double valorporcentagemA = (populaçaoA * porcentagemA) / 100;
 
-      System.out.println("Insira a porcentagem de crescimento populacional do país B: ");
+      System.out.println("Insira a porcentagem de crescimento populacional por ano do país B: ");
       double porcentagemB = scanner.nextDouble();
 
-      double valorporcentagemB = (populaçaoB * porcentagemB) / 100;
-      
+     // double valorporcentagemB = (populaçaoB * porcentagemB) / 100;
 
-      while(populaçaoA < populaçaoB){
+
+      while(populaçaoA <= populaçaoB){
         populaçaoA += populaçaoA * porcentagemA;
         populaçaoB += populaçaoB * porcentagemB;
         anos += 1;
         }
-
-
-
-         
+           
+            System.out.println("Anos para a população A superar a população B:  " + anos);
+            System.out.println("População A: " + populaçaoA);
+            System.out.println("População B: " + populaçaoB);
+        
 
       scanner.close();
     }
