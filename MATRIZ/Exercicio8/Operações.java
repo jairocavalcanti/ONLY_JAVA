fc3aa294b0e5ff9c673585fbd6061e15084af9ca
Jairo <jairoregiscavalcanti@gmail.com>
2023-12-29 00:12:46 -0300

    Exercicio_8_inserido

---
+++ MATRIZ/Exercicio8/Operações.java
@@ -0,0 +1,97 @@
+package MATRIZ.Exercicio8;
+import java.util.Scanner;
+
+public class Operações {
+
+
+   public void preenchers_matriz_1(Scanner scanner,int linhas_1, int colunas_1,int matriz[][]){
+      int cont_1 = 0;
+      System.out.println();
+      System.out.println(" -- MATRIZ_1 -- ");
+      for(int i = 0; i < linhas_1; i++){
+        for(int j = 0; j < colunas_1; j++){
+            cont_1++;
+            System.out.printf(" " + "Insira o elemento de numero #[ " + cont_1 + " ] :");
+            matriz[i][j] = scanner.nextInt();
+        }
+      }
+       System.out.println();
+   }
+
+   public void preencher_matriz_2(Scanner scanner, int linhas_2, int colunas_2, int matriz_2[][]){
+      int cont_2 = 0;
+      System.out.println();
+      System.out.println(" -- MATRIZ_2 -- ");
+      for(int i = 0; i < linhas_2; i++){
+        for(int j = 0; j < colunas_2; j++){
+            cont_2++;
+            System.out.printf(" " + "Insira o elemento de numero #[ " + cont_2 + " ] :");
+            matriz_2[i][j] = scanner.nextInt();
+        }
+      }
+       System.out.println();
+   }
+
+
+  public void mostrar_matriz_1(int linhas_1, int colunas_1, int matriz[][]){
+     System.out.println();
+     System.out.printf(" -- Matriz_1 -- \n");
+     for(int i = 0; i < linhas_1; i++){
+        for(int j = 0; j < colunas_1; j++){
+           System.out.printf(" [ " + matriz[i][j] + " ] ");
+        }
+        System.out.printf("%n");
+    }
+      System.out.println();  
+   }
+
+   
+  public void mostrar_matriz_2(int linhas_2, int colunas_2, int matriz_2[][]){
+     System.out.println();
+     System.out.printf(" -- Matriz_2 -- \n");
+     for(int i = 0; i < linhas_2; i++){
+        for(int j = 0; j < colunas_2; j++){
+           System.out.printf(" [ " + matriz_2[i][j] + " ] ");
+        }
+        System.out.printf("%n");
+    }
+      System.out.println();  
+   }
+
+  public void resultado_soma(int linhas_1, int colunas_1, int linhas_2, int colunas_2, int matriz_1[][], int matriz_2[][], int matriz_soma[][]){  
+   
+    System.out.println();
+    System.out.printf(" -- Matriz resultado: SOMA -- \n");
+    for(int i = 0; i < linhas_1; i++){
+        for(int j = 0; j < colunas_2; j++){
+           System.out.printf(" [ " + matriz_soma[i][j] + " ] ");
+        }
+        System.out.printf("%n"); 
+    }
+    System.out.println();  
+     
+   }
+
+
+
+  public int[][] somarmatrizes(int linhas_1, int colunas_1, int linhas_2, int colunas_2, int matriz_1[][], int matriz_2[][], int matriz_soma[][]){
+    
+   if(linhas_1 == linhas_2 && colunas_1 == colunas_2){
+
+    for(int i= 0; i < linhas_1; i++){
+        for(int j = 0; j < colunas_2; j++){
+          matriz_soma[i][j] = matriz_1[i][j] + matriz_2[i][j]; 
+        }
+      }
+      return matriz_soma;
+   
+    }else{
+   
+    System.out.println("!! Impossível somar matrizes, matrizes acima não possuem as mesmas dimensões !! \n");
+    return null;
+   }
+    
+   
+  }
+
+}
--- MATRIZ/Exercicio8/Principal.java
+++ MATRIZ/Exercicio8/Principal.java
@@ -1,16 +1,66 @@
 package MATRIZ.Exercicio8;
-
 import java.util.Scanner;
+
+ /*
+ Programa para prática de operações matematicas básicas com matrizes 
+ preenchidas pelo usuário 
+ */
 
 public class Principal {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-    
-        
+        Operações ops_2 = new Operações();
+        boolean ops = false;
 
+        System.out.println("Insira o numero de LINHAS da matriz_1: ");
+        int linhas_1 = scanner.nextInt();
 
+        System.out.println("Insira o numero de COLUNAS da matriz_1: ");
+        int colunas_1 = scanner.nextInt();
 
+        System.out.println("Insira o numero de LINHAS da matriz_2: ");
+        int linhas_2 = scanner.nextInt();
 
+        System.out.println("Insira o numero de COLUNAS da matriz_2: ");
+        int colunas_2 = scanner.nextInt();
+
+        int matriz_1[][] = new int[linhas_1][colunas_1];
+        int matriz_2[][] = new int[linhas_2][colunas_2];
+        int matriz_soma[][] = new int[linhas_1][colunas_2];
+
+        ops_2.preenchers_matriz_1(scanner, linhas_1, colunas_1, matriz_1);
+        ops_2.preencher_matriz_2(scanner, linhas_2, colunas_2, matriz_2);
+        ops_2.mostrar_matriz_1(linhas_1, colunas_1, matriz_1);
+        ops_2.mostrar_matriz_2(linhas_2, colunas_2, matriz_2);
+
+        while(ops != true){
+        System.out.println("Insira a operação que deseja realizar com as matrizes: 1 - SOMA // 2 - SUBTRAÇÃO // 3 - MULTIPLICAÇÃO // 0 - ENCERRAR");
+        int opcao = scanner.nextInt();
+
+        switch (opcao) {
+            case 1:
+                System.out.println();
+                System.out.println("Operação de SOMA selecionada! ");
+               
+                break;
+            
+            case 2: 
+                System.out.println("SUBTRAÇÃO !");     
+                break;
+
+            case 3:
+                System.out.println("MULTIPLICAÇÃO !");
+                break;
+           
+            case 0:
+                System.out.println("Programa Encerrado...");
+                return;    
+
+            default:
+                System.out.println("Insira uma opção válida !!");
+                break;
+        }
+    }
 
         scanner.close();
     }
